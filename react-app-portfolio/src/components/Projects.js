bdfa2723c658e71c26dea340795b6912d6b69dd6
rgavin1 <44215916+rgavin1@users.noreply.github.com>
2019-11-08 23:10:17 -0500

    Projects Grids Built

--- react-app-portfolio/src/components/Projects.js
+++ react-app-portfolio/src/components/Projects.js
@@ -1,10 +1,35 @@
-import React from 'react'
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const Projects = () => {
     return (
-        <div>
-            Projects
-        </div>
+        <Container>
+            <h3>Projects</h3>
+            <Row style={{
+                minHeight: '400px',
+                maxHeight: '10em'
+            }}>
+                <Col xs={12} sm={12} md={4} className="bg-primary">1/1</Col>
+                <Col xs={12} sm={12} md={4} className="bg-info">1/2</Col>
+                <Col xs={12} sm={12} md={4} className="bg-danger">1/3</Col>
+            </Row>
+            <Row style={{
+                minHeight: '400px',
+                maxHeight: '10em'
+            }}>
+                <Col xs={12} sm={12} md={4} className="bg-light">2/1</Col>
+                <Col xs={12} sm={12} md={4} className="bg-warning">2/2</Col>
+                <Col xs={12} sm={12} md={4} className="bg-success">2/3</Col>
+            </Row>
+            <Row style={{
+                minHeight: '400px',
+                maxHeight: '10em'
+            }}>
+                <Col xs={12} sm={12} md={4} className="bg-danger">3/1</Col>
+                <Col xs={12} sm={12} md={4} className="bg-dark">3/2</Col>
+                <Col xs={12} sm={12} md={4} className="bg-primary">3/3</Col>
+            </Row>
+        </Container>
     )
 }
 

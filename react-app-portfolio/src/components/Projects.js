67933e1bd79db87e64f28b2c096a8ed90decc084
rgavin1 <44215916+rgavin1@users.noreply.github.com>
2019-11-09 10:52:06 -0500

    Created assets folder with one image

--- react-app-portfolio/src/components/Projects.js
+++ react-app-portfolio/src/components/Projects.js
@@ -2,29 +2,24 @@ import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 
 const Projects = () => {
+    const projectContainer = {
+        minHeight: '400px',
+        maxHeight: '10em'
+    }
     return (
         <Container>
             <h3>Projects</h3>
-            <Row style={{
-                minHeight: '400px',
-                maxHeight: '10em'
-            }}>
+            <Row style={projectContainer}>
                 <Col xs={12} sm={12} md={4} className="bg-primary">1/1</Col>
                 <Col xs={12} sm={12} md={4} className="bg-info">1/2</Col>
                 <Col xs={12} sm={12} md={4} className="bg-danger">1/3</Col>
             </Row>
-            <Row style={{
-                minHeight: '400px',
-                maxHeight: '10em'
-            }}>
+            <Row style={projectContainer}>
                 <Col xs={12} sm={12} md={4} className="bg-light">2/1</Col>
                 <Col xs={12} sm={12} md={4} className="bg-warning">2/2</Col>
                 <Col xs={12} sm={12} md={4} className="bg-success">2/3</Col>
             </Row>
-            <Row style={{
-                minHeight: '400px',
-                maxHeight: '10em'
-            }}>
+            <Row style={projectContainer}>
                 <Col xs={12} sm={12} md={4} className="bg-danger">3/1</Col>
                 <Col xs={12} sm={12} md={4} className="bg-dark">3/2</Col>
                 <Col xs={12} sm={12} md={4} className="bg-primary">3/3</Col>

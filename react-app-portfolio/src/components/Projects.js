4bc528d5b9464dac4c7f86dab133d3c7242428ef
rgavin1 <44215916+rgavin1@users.noreply.github.com>
2019-11-11 22:25:33 -0500

    Fixing Overlay for Images

--- react-app-portfolio/src/components/Projects.js
+++ react-app-portfolio/src/components/Projects.js
@@ -1,47 +1,32 @@
 import React from 'react';
-import { Container, Row, Col, Image, Button } from 'react-bootstrap';
-import Img1 from '../assets/fall-1072821_640.jpg';
-import Img2 from '../assets/bench-560435_640.jpg';
+import { Container, Row, Col, Image, Button, Card } from 'react-bootstrap';
+import realEstateImage from '../assets/real estate web page.png';
+import capstoneProjectImage from '../assets/Capstone.png';
+import webPortfolioImage from '../assets/Web Developer Portfolio.png';
 import '../styles/Projects.css';
 
 const Projects = () => {
     return (
-        <Container >
-            <h3 className="text-center border-bottom ">Projects</h3>
-            <Row className="py-3">
-                <Col className="project" sm={12} md={4}>
-                    <Image src={Img1} fluid />
-                    <div className="projectOverlay text-center">
-                        <h4>Real Estate</h4>
-                        <p>M.E.R.N stack</p>
-                        <Button variant="warning" >Learn More</Button>
-                    </div>
-                </Col>
-                <Col className="project" sm={12} md={4}>
-                    <Image src={Img2} fluid />
-                    <div className="projectOverlay text-center">
-                        <h4>Kent State Parking Meter</h4>
-                        <p>W.A.W.P stack</p>
-                        <Button variant="warning" >Learn More</Button>
-                    </div>
-                </Col>
-                <Col className="project" sm={12} md={4}>
-                    <Image src={Img2} fluid />
-                    <div className="projectOverlay text-center">
-                        <h4>Web Development Portfolio</h4>
-                        <p>React.js</p>
-                        <Button variant="warning" >Learn More</Button>
-                    </div>
-                </Col>
-                <Col className="project" sm={12} md={4}>
-                    <Image src={Img2} fluid />
-                    <div className="projectOverlay text-center">
-                        <h4>Monthly Budget Application</h4>
-                        <p>M.E.R.N stack</p>
-                        <Button variant="warning" >Learn More</Button>
-                    </div>
-                </Col>
-            </Row>
+        <Container> 
+            <h3 className="text-center">Projects</h3>
+            <hr className="py-3" />
+            <Container className="d-flex justify-content-around flex-wrap">
+            <div className="projectStyles" >
+                <Image src={realEstateImage} fluid />
+                <div className="overlay">
+                <h4>Title</h4>
+                <p>Technologies Used</p>
+                </div>
+            </div>
+            <div className="projectStyles">
+                <Image src={capstoneProjectImage} fluid />
+            </div>
+            <div className="projectStyles">
+                <Image src={webPortfolioImage} fluid />
+            </div>
+            <div className="projectStyles" className="bg-light"></div>
+            </Container>
+            <hr className="py-3" />
         </Container>
     )
 }

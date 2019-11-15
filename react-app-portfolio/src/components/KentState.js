e70c5d1fb281c4382bcbdfc5c80147d49e207bcd
rgavin1 <44215916+rgavin1@users.noreply.github.com>
2019-11-14 22:07:09 -0500

    Update to Kent State Project Page - Techology Icon

--- react-app-portfolio/src/components/KentState.js
+++ react-app-portfolio/src/components/KentState.js
@@ -6,10 +6,14 @@ import { Container, Button } from 'react-bootstrap';
 import ScreenShot from '../assets/Capstone1339x615.png';
 
 const KentState = () => {
+    const technologiesStyle = {
+        height: '25px',
+        width: '25px'
+    }
     return (
         <Container className="d-flex projectContainer">
             <Container className="my-auto">
-           <h2 className="text-center" >Kent State Capstone Project</h2>
+           <h2 className="text-center" >Kent State University Parking Meter Application (Capstone Project)</h2>
            <div style={{
                backgroundImage: `url(${ScreenShot})`,
                height: '243px',
@@ -21,20 +25,20 @@ const KentState = () => {
            }}></div>
            <hr className="mt-4"/>
            <Container className="d-flex justify-content-around">
-           <Button title="View" href="" >
+           <Button title="View" href="" className="d-flex">
                <FontAwesomeIcon style={{
                    fontSize: '30px'
-               }} icon={faEye} />
+               }} icon={faEye} /><div className="my-auto ml-2">View</div>
            </Button>
-           <Button title="Repository" href="" >
+           <Button title="Repository" href="" className="d-flex">
                <FontAwesomeIcon style={{
                    fontSize: '30px'
-               }} icon={faGithub} />
+               }} icon={faGithub} /><div className="my-auto ml-2">Github</div>
            </Button>
            </Container>
            <hr className="mt-4" />
            <h3>Description</h3>
-           <p className="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Commodo odio aenean sed adipiscing diam donec adipiscing tristique. Ipsum suspendisse ultrices gravida dictum fusce ut placerat. Amet mauris commodo quis imperdiet massa tincidunt. Gravida rutrum quisque non tellus orci. Fusce ut placerat orci nulla. In pellentesque massa placerat duis ultricies lacus sed turpis tincidunt.
+           <p className="text-justify">Served as a Front-End developer in this capstone project to solve the Kent State University parking meter problem. KSU Commuter students found it difficult locating available parking meters on campus. Our team elected to develop a responsive website that utilizes technologies like HTML, CSS, JavaScript, JSON, PHP, and Google Maps API.
            </p>
            </Container>
            <Container className="my-auto pt-3">
@@ -42,9 +46,9 @@ const KentState = () => {
            <div>
            <h5>Issues</h5>
            <ul>
-           <li>
-                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                   </p>
+              <li>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </p>
                </li>
                <li>
                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -76,37 +80,15 @@ const KentState = () => {
            </Container>
            <hr className="mt-4" />
            <h5 className="text-center mb-3">Technologies</h5>
-           <div className="d-flex justify-content-between">
-               <div style={{
-                   width: '40px',
-                   height: '40px',
-                   backgroundColor: 'darkgray'
-               }}>1</div>
-               <div style={{
-                   width: '40px',
-                   height: '40px',
-                   backgroundColor: 'darkgray'
-               }}>2</div>
-               <div style={{
-                   width: '40px',
-                   height: '40px',
-                   backgroundColor: 'darkgray'
-               }}>3</div>
-               <div style={{
-                   width: '40px',
-                   height: '40px',
-                   backgroundColor: 'darkgray'
-               }}>4</div>
-               <div style={{
-                   width: '40px',
-                   height: '40px',
-                   backgroundColor: 'darkgray'
-               }}>5</div>
-               <div style={{
-                   width: '40px',
-                   height: '40px',
-                   backgroundColor: 'darkgray'
-               }}>6</div>
+           <div className="d-flex justify-content-around">
+           <img style={technologiesStyle} src="https://www.w3.org/html/logo/downloads/HTML5_Badge.svg" alt="HTML5_Badge" title="HTML5" />
+           <img style={technologiesStyle} src="https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg" alt="CSS3  logo" title="CSS3" />  
+           <img style={technologiesStyle} src="https://upload.wikimedia.org/wikipedia/commons/9/99/Unofficial_JavaScript_logo_2.svg" alt="JavaScript Logo" title="JavaScript" />  
+           <img style={technologiesStyle} src="https://upload.wikimedia.org/wikipedia/commons/4/48/GoogleMaps_logo.svg" alt="Google Maps Logo" title="Google Maps" />
+           <img style={technologiesStyle} src="https://www.php.net/images/logos/php-logo.svg" alt="PHP logo" title="PHP" />
+           <img style={technologiesStyle} src="https://upload.wikimedia.org/wikipedia/commons/c/c9/JSON_vector_logo.svg" alt="JSON Logo" title="JSON" />
+           <img style={technologiesStyle} src="https://upload.wikimedia.org/wikipedia/commons/9/93/Amazon_Web_Services_Logo.svg" alt="AWS Web Serivers Logo" title="Amazon Web Services" />
+           <img style={technologiesStyle} src="https://upload.wikimedia.org/wikipedia/de/d/dd/MySQL_logo.svg" alt="MySQL Logo" title="MySQL" />
            </div>
            </Container>
         </Container>

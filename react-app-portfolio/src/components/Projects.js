5a64a6214d11f2705ec7ae47c549811c49f31b3e
rgavin1 <44215916+rgavin1@users.noreply.github.com>
2019-11-18 22:04:24 -0500

    Update Links

--- react-app-portfolio/src/components/Projects.js
+++ react-app-portfolio/src/components/Projects.js
@@ -6,7 +6,7 @@ import webPortfolioImage from '../assets/Web Developer Portfolio.png';
 import RestaurantScreenShot from '../assets/Resturant.png';
 import '../styles/Projects.css';
 import { TweenMax } from 'gsap';
-
+import { Link } from 'react-router-dom';
 
 const Projects = () => {
     let leftSide = useRef(null);
@@ -22,20 +22,22 @@ const Projects = () => {
             <hr className="py-3" />
             <Container className="d-flex justify-content-around flex-wrap">
             <div ref={(element) => leftSide = element} title="Real Estate Project" className="projectStyles shadow rounded" >
-                <Image src={realEstateImage} fluid />
-                <div className="overlay">
-                <h4>Title</h4>
-                <p>Technologies Used</p>
-                </div>
+                <Link to="/RealEstateProj">
+                    <Image src={realEstateImage} fluid />
+                        <div className="overlay">
+                        <h4>Title</h4>
+                        <p>Technologies Used</p>
+                        </div>
+                </Link>
             </div>
             <div ref={(element) => rightSide = element} title="Capstone Project" className="projectStyles shadow rounded" rounded>
-                <Image src={capstoneProjectImage} fluid />
+                <Link to="/CapstoneProj"><Image src={capstoneProjectImage} fluid /></Link>
             </div>
             <div title="Web Portfolio Project" className="projectStyles shadow rounded" rounded>
-                <Image src={webPortfolioImage} fluid />
+                <Link to="/PortfolioProj" ><Image src={webPortfolioImage} fluid /></Link>
             </div>
             <div title="Restaurant Project" className="projectStyles shadow rounded" rounded>
-                <Image src={RestaurantScreenShot} fluid />
+                <Link to="/RestaurantProj" ><Image src={RestaurantScreenShot} fluid /></Link>
             </div>
             </Container>
             <hr className="py-3" />

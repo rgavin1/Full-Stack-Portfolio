2fd4e6392e7cc6e5f82af4c5c02eb63f589d2d65
rgavin1 <44215916+rgavin1@users.noreply.github.com>
2019-11-20 21:36:09 -0500

    Updated Nest Page

--- react-app-portfolio/src/components/Hero.js
+++ react-app-portfolio/src/components/Hero.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React from 'react';
 import { Jumbotron, Container } from 'react-bootstrap';
 import '../styles/Hero.css';
 import ProfilePic from '../assets/profile pic.jpg';
--- react-app-portfolio/src/components/RealEstateProject.js
+++ react-app-portfolio/src/components/RealEstateProject.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container, Button } from 'react-bootstrap';
-import { faEye } from '@fortawesome/free-solid-svg-icons';
+import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import '../styles/RealEstateProject.css';
@@ -10,8 +10,8 @@ import ReactLogo from '../assets/reactjs-icon.svg';
 
 const RealEstateProject = () => {
     const technologiesStyle = {
-        height: '25px',
-        width: '25px'
+        height: '30px',
+        width: '30px'
     }
     return (
         <Container className="d-flex projectContainer mt-3">
@@ -32,7 +32,7 @@ const RealEstateProject = () => {
            <Button title="View" href="" className="d-flex">
                <FontAwesomeIcon style={{
                    fontSize: '30px'
-               }} icon={faEye} /><div className="my-auto ml-2">View</div>
+               }} icon={faExternalLinkAlt} /><div className="my-auto ml-2">View</div>
            </Button>
            <Button title="Repository" href="" className="d-flex">
                <FontAwesomeIcon style={{
@@ -42,55 +42,63 @@ const RealEstateProject = () => {
            </Container>
            <hr className="mt-4" />
            <h3>Description</h3>
-           <p className="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Commodo odio aenean sed adipiscing diam donec adipiscing tristique. Ipsum suspendisse ultrices gravida dictum fusce ut placerat. Amet mauris commodo quis imperdiet massa tincidunt. Gravida rutrum quisque non tellus orci. Fusce ut placerat orci nulla. In pellentesque massa placerat duis ultricies lacus sed turpis tincidunt.
+           <p className="text-justify">This Real Estate web application allowed me to develop skills, learn new concepts, and study unfamiliar technologies. The Nest web application was created to assist in my learning of the M.E.R.N tech stack. I selected the best real estate applications available (i.e., Zillow, Redfin, and Trulia) and attempted to construct a similar product. The objective of this project was to jump into messy real-world problems and solve for all the unknowns.
            </p>
            </Container>
            <Container className="my-auto pt-3">
            <Container className="justify-content-between">
            <div>
            <h5>Issues</h5>
-           <ul>
-           <li>
-                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                   </p>
-               </li>
-               <li>
-                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                   </p>
-               </li>
-               <li>
-                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                   </p>
-               </li>
-           </ul>
+           <ol>
+            <li>
+                <p>Unfamiliar with setting up an Express.js server and establishing a connection with MongoDB Atlas using Node.js.
+                </p>
+            </li>
+            <li>
+                <p>Storage of high-quality images in MongoDB without occupying an excessive amount of space in the database.
+                </p>
+            </li>
+            <li>
+                <p>Understanding the React-Router-Dom with the goal I could link to other documents and components.
+                </p>
+            </li>
+            </ol>
            </div>
            <div>
            <h5>Solutions</h5>
-           <ul>
+           <ol>
                <li>
-                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                   <p>Read the documentation for setting up an Express server and making a request and receiving a response. 
                    </p>
                </li>
                <li>
-                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                   <p>Solved the storage issues by storing only the file paths of the images in the database.
                    </p>
                </li>
                <li>
-                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                   <p>Read the documentation for NPM React-Router-Dom and viewed tutorials, which tackled the issue.
                    </p>
                </li>
-           </ul>
+           </ol>
            </div>
            </Container>
            <hr className="mt-4" />
            <h5 className="text-center mb-3">Technologies</h5>
-           <div className="d-flex justify-content-around">
+           <div className="d-flex justify-content-around flex-wrap">
            <img style={technologiesStyle} src="https://www.w3.org/html/logo/downloads/HTML5_Badge.svg" alt="HTML5_Badge" title="HTML5" />
         <img style={technologiesStyle} src="https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg" alt="CSS3  logo" title="CSS3" />  
         <img style={technologiesStyle} src={BootstrapLogo} alt="Bootstrap B Solid Logo" title="Bootstrap" />
         <img style={technologiesStyle} src="https://upload.wikimedia.org/wikipedia/commons/9/99/Unofficial_JavaScript_logo_2.svg" alt="JavaScript Logo" title="JavaScript" />
         <img style={technologiesStyle} src={ReactLogo} alt="Reactjs Logo" title="React" />
         <img style={technologiesStyle} src="https://upload.wikimedia.org/wikipedia/commons/4/48/GoogleMaps_logo.svg" alt="Google Maps Logo" title="Google Maps" />
+        <img style={{
+            height: '30px',
+            width: '50px'
+        }} src="https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg" alt="Node Js Logo" title="Nodejs" />
+        <img style={{
+            height: '20px',
+            width: '80px'
+        }} src="https://upload.wikimedia.org/wikipedia/commons/9/93/MongoDB_Logo.svg" alt="MongoDB" title="MongoDB" />
            </div>
            </Container>
         </Container>
